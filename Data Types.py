918a802d2e85d0f08da882ad8ff4dd406f588b51
aweshkhan <awesh.persec@gmail.com>
2020-09-18 18:56:35 +0530

    intial commit

---
+++ Data Types.py
@@ -0,0 +1,30 @@
+a = 5
+
+print(type(a))
+
+print(type(5.0))
+
+c = 5 + 3j
+print(c + 3)
+
+print(isinstance(c, complex))
+# Output: 107
+print(0b11)
+print(0b1)
+print(1 + 2.0)
+3.0
+print(int(2.3))
+2
+print(int(-2.8))
+-2
+print(float(5))
+5.0
+print(complex('3+5j'))
+(3+5j)
+print(1.1+2.2)== 3.3
+import decimal
+print(0.1)
+print(decimal.Decimal(0.1))
+from decimal import Decimal as a
+print(a('1.1') + a('5.5'))
+print(a(1.1) + a(5.5))
