9b38aec052f5de0f7648aa7424f662e52039b665
aweshkhan <awesh.persec@gmail.com>
2020-09-21 20:04:04 +0530

    while loop ready uploaded in git

---
+++ whileloops.py
@@ -0,0 +1,22 @@
+i = 1200
+while i > 206:
+  print(i)
+  if i == 2000:
+      break
+  i += 100
+i = 1200
+while i < 2006:
+  i += 2
+  if i == 2004:
+      continue
+  print(i)
+
+i = 1
+while i < 10:
+    print(i)
+    i += 1
+    if i == 5:
+        continue
+    print(i)
+else:
+    print("python experrts")
